afe4fb567ab919a987d8f579afef534bd2e33c63
vasanthaimaster <bettepuvasanthabhanu@gmail.com>
2025-08-02 13:32:52 +0530

    chatbackend.py

--- chatbackend.py
+++ chatbackend.py
@@ -1,38 +1,33 @@
-# 1. Import langchain functions
 from langchain_community.chat_models import BedrockChat
-# from langchain.memory import ConversationSummaryBufferMemory
-from langchain.memory import ConversationBufferMemory
+from langchain.memory import ConversationSummaryBufferMemory
 from langchain.chains import ConversationChain
-import transformers
-# 2. Create function to invoke model 
+
 def titan_llm():
-    llm = ChatBedrock(
+    llm = BedrockChat(
         model_id="amazon.titan-text-express-v1",
+        region_name="us-east-1",  # Replace with your AWS region
         model_kwargs={
             "temperature": 0.5,
-            "topP": 1, 
-            "maxTokenCount": 100  
-        },
+            "topP": 1,
+            "maxTokenCount": 100
+        }
     )
-    return llm  
+    return llm
 
-# create a memory function for the chatbot
 def create_memory():
-    llm = titan_llm()  
-    memory = ConversationBufferMemory(
+    llm = titan_llm()
+    memory = ConversationSummaryBufferMemory(
         llm=llm,
         max_token_limit=256,
     )
     return memory
 
-# create a chat client function to run the chatbot
 def get_chat_response(input_text, memory):
-    llm = titan_llm() 
+    llm = titan_llm()
     conversation = ConversationChain(
-        llm=llm,       
+        llm=llm,
         verbose=True,
         memory=memory,
     )
     chat_response = conversation.invoke(input=input_text)
-
-    return chat_response['response'] 
+    return chat_response['response']
